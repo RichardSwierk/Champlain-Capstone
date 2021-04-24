0845483d15f12f5dd41dc619289cc0bef1b67286
Richard T Swierk <richard.swierk@mymail.champlain.edu>
2021-04-24 08:16:14 -0400

    Delete setup.sh

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo apt-get install update
-sudo apt-get install python
-sudo apt-get install libhidapi-hidraw0
-sudo apt-get install python-pip
-sudo pip install pyusb
-sudo pip install hid
